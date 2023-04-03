9caeb77c50f18c248a7f1e5d1717b2c491f3843d
Ori Baram <obrm770@gmail.com>
2023-04-03 15:01:50 +0300

    add installation prompt

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import useInstallPrompt from './hooks/useInstallPrompt';
+
 import CurrencyConverter from './pages/CurrencyConverter';
 
 function App() {
+  useInstallPrompt()
+
   return (
     <div className='App'>
       <CurrencyConverter /> 
